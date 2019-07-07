4c73ce3716720198a1c759f567cf0a92577ad903
utayazilim <utayazilim@hotmail.com>
2019-07-07 22:31:11 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('sass',function(){
-    gulp.src('./src/scss/**/*.scss')
+    return gulp.src('./src/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./src/css'));
 });
