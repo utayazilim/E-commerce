42821090acbc1aedb3c22c1f1f276b47ad06e51f
utayazilim <utayazilim@hotmail.com>
2019-07-07 20:06:37 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const cleanCSS = require("gulp-clean-css");
-const browserSync = require("browser-sync").create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('watch', function () {
-    browserSync.init({
-        server:{
-            baseDir:'./main'            
-        }
-    })
-    gulp.watch('./scss/**/*.scss').on('change',browserSync.reload);
-    gulp.watch('./main/*.html').on('change',browserSync.reload);
-  });
+gulp.task('sass',function(){
+    gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe('./css');
+});
 
-
-
+gulp.task('watch',function(){
+    gulp.watch('./scss/**/*.scss',['sass']);
+});
